08f4e7d2e3a3cfcf21ebe00e4ac079cc41ac28e9
mahmoud <eslamimahmoud98@gmail.com>
2024-02-22 16:08:44 +0330

    feat: add `onSelected` to access to the selected icon

--- lib/src/widgets/base_widget_builder.dart
+++ lib/src/widgets/base_widget_builder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:icon_gallery/src/data/data_builder.dart';
 
 class BaseWidgetBuilder<T> extends StatelessWidget {
@@ -7,36 +6,45 @@ class BaseWidgetBuilder<T> extends StatelessWidget {
     super.key,
     required this.items,
     required this.optionBuilders,
+    this.onSelected,
   });
 
   final List<IconValue<T>> items;
   final List<OptionBuilder<T>> optionBuilders;
+  final ValueChanged<IconValue<T>>? onSelected;
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      alignment: WrapAlignment.spaceAround,
-      children: List.generate(
-        optionBuilders.length,
-        (optionIndex) => Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (optionBuilders[optionIndex].title != null) // Conditional check
-              optionBuilders[optionIndex].title!,
-            Wrap(
-              children: List.generate(
-                optionBuilders[optionIndex].options.length,
-                (index) => Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-                  child: optionBuilders[optionIndex].widgetBuilder(
-                    context,
-                    optionBuilders[optionIndex].options[index],
-                  ),
-                ),
-              ),
-            ),
-          ],
+      alignment: WrapAlignment.center,
+      children: optionBuilders
+          .map((builder) => _buildOptionColumn(context, builder))
+          .toList(),
+    );
+  }
+
+  Widget _buildOptionColumn(BuildContext context, OptionBuilder<T> builder) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (builder.title != null) builder.title!,
+        Wrap(
+          alignment: WrapAlignment.start,
+          children: builder.options
+              .map((option) => _buildOption(context, builder, option))
+              .toList(),
         ),
+      ],
+    );
+  }
+
+  Widget _buildOption(
+      BuildContext context, OptionBuilder<T> builder, IconValue<T> option) {
+    return InkWell(
+      onTap: () => onSelected?.call(option),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+        child: builder.widgetBuilder(context, option),
       ),
     );
   }
